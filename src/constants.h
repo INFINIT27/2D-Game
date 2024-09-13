67737461425f977ec4369fffd6ed9626a441338c
Redi Lleshi <redilleshi52@gmail.com>
2024-09-13 19:19:05 -0400

    Updated Game Dimensions

--- src/constants.h
+++ src/constants.h
@@ -1,13 +1,13 @@
 #pragma once
 
-const int SCREEN_WIDTH = 1000;
-const int SCREEN_HEIGHT = 500;
+const int SCREEN_WIDTH = 1500;
+const int SCREEN_HEIGHT = 750;
 const int STARTING_ROW = 43;
 const int STARTING_COL = 4;
 
 const int ROWS = 50;
 const int COLS = 100;
-const int LENGTH = 10;
+const int LENGTH = 15;
 
 const int PLAYER_COLOR = 2;
 const int BACKGROUND_COLOR = 0;
